bf698d2cbe7d63c8a96f3ecc56e847d564b887e8
Kittinan <azazxxc147896325@gmail.com>
2026-02-09 12:28:21 +0700

    Delete 385Week4 directory

--- 385Week4/Workshop3/Workshop3.1/W3-1.js
+++
@@ -1,6 +0,0 @@
-const operation = require('./operation');
-
-console.log(operation('add',4,5));
-console.log(operation('subtract',10,3));
-console.log(operation('multiply',5,6));
-console.log(operation('divide',8,2));
